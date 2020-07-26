a74adc2481ea1c13a9648cd4c22072747ce7d6b8
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-26 16:32:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
+#Retrieve attributes of CSS elements
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+doc.css(".headline-26OIBN").text
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN").text
 
+#Iterate over CSS elements
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
