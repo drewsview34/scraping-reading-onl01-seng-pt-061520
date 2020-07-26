41e5bac7b5a2f1f0c7965cdebf4e476ae80d230e
Andrew Hinojosa <drewsview34@outlook.com>
2020-07-26 16:39:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,3 +11,7 @@ doc.css(".headline-26OIBN").text
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each do |course|
+  puts course.text.strip
+end
